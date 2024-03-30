4b3fc75b2b8da6626e5a52f61ccb13cd803032dd
ComfyJace <jason_erwin_dimalanta@dlsu.edu.ph>
2024-03-30 21:54:57 +0800

    fix: floating point iteration

--- asmfunc.asm
+++ asmfunc.asm
@@ -16,12 +16,12 @@ section .bss
     
 section .data
 
-vec_A       dd 1.0, 2.0, 3.0, 4.0
-vec_B       dd 2.0, 3.0, 4.0, 5.0
-n           dd 4
+vec_A       dq 1.9, 2.0, 3.0, 4.0
+vec_B       dq 2.0, 3.0, 4.0, 5.5
+n           dq 4
 sdot        dq 0.0
 
-msg_1       db "Dot product (sdot): %f",10,0
+msg_1       db "Dot product (sdot): %.2f",10,0
 
 section .text
 extern printf
@@ -32,32 +32,29 @@ main:
     
     sub rsp, 8*4 ; 2 because only 2 parameter
     
-    movss xmm0, [zero]
-    mov rax, vec_A 
-    mov rbx, vec_B
+    lea rsi, [vec_A]
+    lea rdi, [vec_B]
     mov rcx, [n] ; counter (length)
     
+    xorpd xmm0, xmm0
     ; TODO: Dot product loop
 getDotProd:
-    movss xmm1, dword [rax] 
-    movss xmm2, dword [rbx]
-    mulss xmm1, xmm2        ; A[i] x B[i]
-    addss xmm0, xmm1        ; sum
-    add rax, 4              ; next float
-    add rbx, 4              ; next float
-    dec rcx                 ; - counter
-    jnz getDotProd
+    movsd xmm1, qword [rsi]
+    movsd xmm2, qword [rdi]
+    mulsd xmm1, xmm2
+    addsd xmm0, xmm1
+    add rsi, 8
+    add rdi, 8
+    loop getDotProd
     
-    ; TODO: Store in sdot
-    movss dword [sdot], xmm0
     
-    ; TODO: Display sdot
-    xor rcx, rcx ; clear for parameter use
+    movsd qword [sdot], xmm0
+    xor rcx, rcx
     xor rdx, rdx
-   
-    mov rcx, msg_1
+    
+    ;Print 
+    lea rcx, [msg_1]
     mov rdx, [sdot]
     call printf
-    
     add rsp, 8*4
     ret
