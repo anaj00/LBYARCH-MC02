3c986712e3ce74ab6de759317ec072216a7db73b
anaj00 <bantolino.jana@gmail.com>
2024-04-01 00:02:02 +0800

    feat: changed loop to exp

--- MCO2_BantolinoDimalanta/main.c
+++ MCO2_BantolinoDimalanta/main.c
@@ -7,39 +7,32 @@
 extern double getDotProduct(double* vec_A, double* vec_B, long long int size_n);
 
 int main() {
-    long long int size_n;
-
     // Loop through different vector sizes
-    for (size_n = 1048576; size_n <= 1048579; size_n += 2) {
+    for (int exp = 20; exp <= 28; exp += 2) {
+        int size_n = pow(2, exp);
+
         clock_t start, end, c_duration, asm_duration;
-        double sdot_c, sdot_asm;
+        double sdot_c = 0.0, sdot_asm = 0.0;
         double loop_time = 0.0;
         double c_time = 0.0;
         
-        printf("Vector size: %lld\n", size_n);
+        printf("Vector size: %d^%d (%d)\n", 2, exp, size_n);
         // Allocate memory for vec_A and vec_B
         double* vec_A = (double*)malloc(size_n * sizeof(double));
         double* vec_B = (double*)malloc(size_n * sizeof(double));
-        
-        // Initialize vec_A and vec_B with random values
-        printf("vec_A: [ ");
-        for (int i = 0; i < size_n; ++i) {
-            vec_A[i] = (double)rand() / RAND_MAX; // Random value between 0 and 1
-            // printf(" %.2f", vec_A[i]); // Print each element of vec_A
-        }
-        printf(" ]\n\n");
 
-        printf("vec_B: [");
-        for (int i = 0; i < size_n; ++i) {
-            vec_B[i] = (double)rand() / RAND_MAX; // Random value between 0 and 1
-            // printf(" %.2f", vec_B[i]); // Print each element of vec_B
+        // Initialize vec_A and vec_B with random values
+        for (int i = 0; i < size_n; i++) {
+            vec_A[i] = (double)rand(); // Random value between 0 and 1
         }
-        printf("] \n\n");
+
+        for (int i = 0; i < size_n; i++) {
+            vec_B[i] = (double)rand(); // Random value between 0 and 1
+        }
 
         // Time the C version
         start = clock();
         for (int j = 0; j < 30; j++) {
-            // printf("-----------[%d]-----------\n", j);
             sdot_c = 0.0;
             for (int k = 0; k < size_n; k++) {
                 sdot_c += vec_A[k] * vec_B[k];
@@ -47,23 +40,21 @@ int main() {
         } 
         end = clock();
         c_duration = (end - start);
-        loop_time = (double)c_duration*1e3/CLOCKS_PER_SEC;
-        //average       
-        c_time = (double)loop_time / 30.0;
-        printf("C version: %f nanosconds on average, dot product: %.2lf\n", (float)c_duration, sdot_c);
+        loop_time = (double)c_duration*1e3/CLOCKS_PER_SEC;   
+        c_time = (double)loop_time / 30.0; // average    
+        printf("C version: %f ms on average, dot product: %.2lf\n", (float)c_duration, sdot_c);
 
         // Time the assembly version
         double asm_time = 0.0;
         start = clock();
         for (int j = 0; j < 30; j++) {
-            // printf("-----------[%d]-----------\n", j);
             sdot_asm = getDotProduct(vec_A, vec_B, size_n);
         }
         end = clock();
         asm_duration = (end - start);
         loop_time = (double)asm_duration*1e3 / CLOCKS_PER_SEC;
         asm_time = (double)loop_time / 30.0;
-        printf("Assembly version: %f nanosconds on average, dot product: %.2lf\n\n\n", (float)asm_duration, sdot_asm);
+        printf("Assembly version: %f nanosconds on average, dot product: %.2lf\n\n", (float)asm_duration, sdot_asm);
 
         // Free dynamically allocated memory
         free(vec_A);
